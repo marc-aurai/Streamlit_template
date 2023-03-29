e352ea707212554ca8e5844d09538e77a58f6f3e
marc-aurai <marc@aurai.com>
2023-03-29 16:48:20 +0200

    black format -> connector

--- opta/connector.py
+++ opta/connector.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import requests
-from utils.opta_feeds import (get_matchstats_cards, get_matchstats_goals,
-                              get_tournamentschedule)
+from utils.opta_feeds import (
+    get_matchstats_cards,
+    get_matchstats_goals,
+    get_tournamentschedule,
+)
 
 
 def get_espn_data(csv_name: str) -> pd.DataFrame:
@@ -12,15 +15,32 @@ def get_espn_data(csv_name: str) -> pd.DataFrame:
 
 
 def refactor_df(df_tournament: pd.DataFrame) -> pd.DataFrame:
-    df_tournament['awayContestantCode'] = df_tournament['awayContestantCode'].str.replace('NEC', 'N.E.C.', regex=True)
-    df_tournament['homeContestantCode'] = df_tournament['homeContestantCode'].str.replace('NEC', 'N.E.C.', regex=True)
-    df_tournament['awayContestantCode'] = df_tournament['awayContestantCode'].str.replace('AJX', 'AJA', regex=True)
-    df_tournament['homeContestantCode'] = df_tournament['homeContestantCode'].str.replace('AJX', 'AJA', regex=True)
+    df_tournament["awayContestantCode"] = df_tournament[
+        "awayContestantCode"
+    ].str.replace("NEC", "N.E.C.", regex=True)
+    df_tournament["homeContestantCode"] = df_tournament[
+        "homeContestantCode"
+    ].str.replace("NEC", "N.E.C.", regex=True)
+    df_tournament["awayContestantCode"] = df_tournament[
+        "awayContestantCode"
+    ].str.replace("AJX", "AJA", regex=True)
+    df_tournament["homeContestantCode"] = df_tournament[
+        "homeContestantCode"
+    ].str.replace("AJX", "AJA", regex=True)
     return df_tournament
 
+
 def merge(df_espn: pd.DataFrame, df_tournament: pd.DataFrame) -> pd.DataFrame:
-    df_merged = pd.merge(df_espn, df_tournament, how='left', left_on=['date', 'home_abbrev', 'away_abbrev'], right_on=['date', 'homeContestantCode', 'awayContestantCode'])
-    df_merged.drop(df_merged.filter(regex="Unname"),axis=1, inplace=True) # Drop unnamed columns
+    df_merged = pd.merge(
+        df_espn,
+        df_tournament,
+        how="left",
+        left_on=["date", "home_abbrev", "away_abbrev"],
+        right_on=["date", "homeContestantCode", "awayContestantCode"],
+    )
+    df_merged.drop(
+        df_merged.filter(regex="Unname"), axis=1, inplace=True
+    )  # Drop unnamed columns
     return df_merged
 
 
@@ -33,7 +53,9 @@ if __name__ == "__main__":
 
     df_merged = merge(df_espn, df_goals)
 
-    df_tournament.to_csv("./opta/data/{}_{}.csv".format(table_kind, competition), sep=";")
+    df_tournament.to_csv(
+        "./opta/data/{}_{}.csv".format(table_kind, competition), sep=";"
+    )
     df_merged.to_csv("./opta/data/merged/merged_{}.csv".format(table_kind), sep=";")
     df_cards.to_csv("./opta/data/merged/cards.csv", sep=";")
     df_goals.to_csv("./opta/data/merged/goals.csv", sep=";")
