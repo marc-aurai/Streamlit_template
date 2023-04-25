1d4086025e8c04be4ddbdf1c2c998acf0d869282
marc-aurai <marc@aurai.com>
2023-04-25 16:28:20 +0200

    Test video

--- pages/Video.py
+++ pages/Video.py
@@ -26,11 +26,12 @@ all_videos = []
 prefix = "test_videos_streamlit/"
 for objects in gpt_ai_bucket.objects.filter(Prefix=prefix):
     all_videos.append(str(objects.key).replace(prefix, ''))
+all_videos = ' '.join(all_videos).split()
 
 selected_video = st.selectbox(
             "Wedstrijd datum: ", all_videos
         )
-st.writ(selected_video)
+st.write(selected_video)
 
 obj = s3.Object("gpt-ai-tool-wsc", "test_videos_streamlit/{}".format(selected_video))
 body = obj.get()['Body'].read()
