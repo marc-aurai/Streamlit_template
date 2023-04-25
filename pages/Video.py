aa19e5adc1fee6af7727bdda3593a6d6cefad6a0
marc-aurai <marc@aurai.com>
2023-04-25 16:24:54 +0200

    test video streamlit

--- pages/Video.py
+++ pages/Video.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import boto3
 
-s3 = boto3.client('s3')
-# gpt_ai_bucket = s3.Bucket('gpt-ai-tool-wsc')
+s3 = boto3.resource('s3')
+gpt_ai_bucket = s3.Bucket('gpt-ai-tool-wsc')
 
 def streamlit_page_config():
     st.set_page_config(
@@ -23,15 +23,12 @@ streamlit_page_config()
 st.sidebar.success("Bekijk een video op deze pagina.")
 
 all_videos = []
-response = s3.list_objects_v2(
-    Bucket='gpt-ai-tool-wsc',
-    Prefix='test_videos_streamlit/')
-
-for content in response.get('Contents', []):
-    all_videos.append(content['Key'])
+prefix = "test_videos_streamlit/"
+for objects in gpt_ai_bucket.objects.filter(Prefix=prefix):
+    all_videos.append(str(objects.key).replace(prefix, ''))
 
 selected_video = st.selectbox(
-            "Video: ", all_videos
+            "Wedstrijd datum: ", all_videos
         )
 
 obj = s3.Object("gpt-ai-tool-wsc", "test_videos_streamlit/{}".format(selected_video))
