3a80a568a39e699965e0fcfe81aee1ecba981d46
marc-aurai <marc@aurai.com>
2023-04-26 15:05:30 +0200

    Update AWS Script Eredivisie

--- AWS_scripts/dataset_pipeline/eredivisie.sh
+++ AWS_scripts/dataset_pipeline/eredivisie.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python /home/app/opta/soccer_pipeline.py --competitie_name eredivisie_AWS --competitie_id d1k1pqdg2yvw8e8my74yvrdw4 --outletAuthKey outletAuthKey_ereD
+python3 /home/app/opta/soccer_pipeline.py --competitie_name eredivisie_AWS --competitie_id d1k1pqdg2yvw8e8my74yvrdw4 --outletAuthKey outletAuthKey_ereD
