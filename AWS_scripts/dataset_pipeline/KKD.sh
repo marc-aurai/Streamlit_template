ac3165540375561029b62918cab07cd540d5f689
marc-aurai <marc@aurai.com>
2023-04-28 14:46:30 +0200

    Add rm command in shell scripts. (AWS)

--- AWS_scripts/dataset_pipeline/KKD.sh
+++ AWS_scripts/dataset_pipeline/KKD.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 python3 /home/app/opta/soccer_pipeline.py --folder_name All --competitie_name KKD_22-23 --competitie_id 1w03tfpd5nc6woo8j2nc9239w --outletAuthKey outletAuthKey_KKD
+rm -rf /home/app/pages/data/*
