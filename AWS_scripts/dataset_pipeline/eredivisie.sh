96c5200662c39c11fc40afa424c29fdb56cbd892
marc-aurai <marc@aurai.com>
2023-04-28 14:17:16 +0200

    Create one folder

--- AWS_scripts/dataset_pipeline/eredivisie.sh
+++ AWS_scripts/dataset_pipeline/eredivisie.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 /home/app/opta/soccer_pipeline.py --folder_name Eredivisie --competitie_name eredivisie_22-23 --competitie_id d1k1pqdg2yvw8e8my74yvrdw4 --outletAuthKey outletAuthKey_ereD
-python3 /home/app/opta/soccer_pipeline.py --folder_name Eredivisie --competitie_name eredivisie_21-22 --competitie_id dp0vwa5cfgx2e733gg98gfhg4 --outletAuthKey outletAuthKey_ereD
+python3 /home/app/opta/soccer_pipeline.py --folder_name All --competitie_name eredivisie_22-23 --competitie_id d1k1pqdg2yvw8e8my74yvrdw4 --outletAuthKey outletAuthKey_ereD
+python3 /home/app/opta/soccer_pipeline.py --folder_name All --competitie_name eredivisie_21-22 --competitie_id dp0vwa5cfgx2e733gg98gfhg4 --outletAuthKey outletAuthKey_ereD
