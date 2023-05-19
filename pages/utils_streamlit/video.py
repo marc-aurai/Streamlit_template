a0e4763995cd484f98152ddbd4bf4467806eb52c
marc-aurai <marc@aurai.com>
2023-05-19 16:35:07 +0200

    Test Video page

--- pages/utils_streamlit/video.py
+++ pages/utils_streamlit/video.py
@@ -3,6 +3,7 @@ import os
 import streamlit as st
 import json
 import pandas as pd
+import datetime as dt
 
 
 def videoMetaData():
@@ -29,16 +30,24 @@ def videoMetaData():
 def ST_readVideo():
     try:
         df, s3 = videoMetaData()
-        selectedTitle = st.selectbox(
-                    "Selecteer video: ", df.title.values
-                )
-        selected_video = df.loc[df["title"] == selectedTitle]
+        df["date"] = pd.to_datetime(df["date"], format="%Y-%m-%dT%H:%M:%S.%f").dt.strftime('%a %d %b %Y')
+        sorted_dates = sorted(df.date.unique().tolist(), key=lambda x: dt.datetime.strptime(x, '%a %d %b %Y'), reverse=True)
 
+        selected_date = st.selectbox(
+                    "Selecteer datum: ", sorted_dates
+                )
+        df_date = df.loc[df["date"] == selected_date]
+
+
+        selectedTitle = st.selectbox(
+                    "Selecteer video: ", df_date.title.values
+                )
+        selected_video = df_date.loc[df_date["title"] == selectedTitle]
         try:
             obj = s3.Object("wsc-espn-site", "uploads/"+str(selected_video.filename.values[0])+".mp4")
             body = obj.get()['Body'].read()
             st.video(body)
         except:
             st.write("Video not supported.")
-    except:
-        st.write("Initialisatie nog bezig.")
+    except: 
+        st.write("Aub wacht even, nog aan het initialiseren..")
