64625478037a3505af6a6e97bdf148fd0b06c38b
marc-aurai <marc@aurai.com>
2023-05-30 10:25:07 +0200

    Add selectbox to formations

--- pages/utils_streamlit/stats.py
+++ pages/utils_streamlit/stats.py
@@ -15,127 +15,134 @@ def ST_showFormation(
     df: pd.DataFrame,
 ) -> str:
     with select_field:
-        
-        df.rename(
-            columns={"playerName": "Naam",
-                    "position": "Positie",
-                    "positionSide": "Positie kant"},
-            inplace=True,
+        selected_formations = st.checkbox(
+            value=True,
+            label="Opstelling van:\n{}".format(
+                df_match_selected[str(team) + "_team"].values[0],
+            ),
         )
-        gd = GridOptionsBuilder.from_dataframe(
-            df[["Naam", "Positie", "Positie kant"]]
-        )
-        gd.configure_pagination(enabled=False)
-        gd.configure_selection(selection_mode="single", use_checkbox=True)
-        gridOptions = gd.build()
-
-        custom_css = {
-            ".ag-column-hover": {"background-color": "#Cbcbcb", "color": "#FFFFFF"},
-            ".ag-row-hover": {"background-color": "#Cbcbcb", "color": "#FFFFFF"},
-            ".ag-header-hover": {"background-color": "#Cbcbcb", "color": "#FFFFFF"},
-            ".ag-header-cell": {"background-color": "#100c44", "color": "#FFFFFF"},
-            ".ag-row-odd": {"background-color": "#100c44", "color": "#FFFFFF"},
-            ".ag-row-even": {"background-color": "#100c44", "color": "#FFFFFF"},
-            ".ag-subheader": {"border-color": "#100c44"},
-        }
-
-        grid_table = AgGrid(
-            df[["Naam", "Positie", "Positie kant"]],
-            gridOptions=gridOptions,
-            enable_enterprise_modules=False,
-            fit_columns_on_grid_load=True,
-            custom_css=custom_css,
-            update_mode=GridUpdateMode.MODEL_CHANGED,
-            allow_unsafe_jscode=True,
-            reload_data=True,
-        )
-        # Show substitutions
-        substitutions = df_match_selected["substitutions_" + str(team)].values[0]
-        nameOff, symbol, nameOn, timeMin = st.columns((4, 1, 4, 1))
-        if substitutions:
-            for substitution in ast.literal_eval(substitutions):
-                with nameOff:
-                    st.write(
-                        "<span style='font-size:12px'>{}</span>".format(
-                            substitution["playerOffName"]
-                        )
-                        + "<span style='color:red;font-size:12px'>{}</span>".format(
-                            "⬇"
-                        ),
-                        unsafe_allow_html=True,
-                    )
-                with symbol:
-                    st.write(
-                        "<span style='color:white;font-size:12px'>{}</span>".format(
-                            "⇆"
-                        ),
-                        unsafe_allow_html=True,
-                    )
-                with nameOn:
-                    st.write(
-                        "<span style='font-size:12px'>{}</span>".format(
-                            substitution["playerOnName"]
-                        )
-                        + "<span style='color:green;font-size:12px'>{}</span>".format(
-                            "⬆"
-                        ),
-                        unsafe_allow_html=True,
-                    )
-                with timeMin:
-                    st.write(
-                        "<span style='color:white;font-size:12px'>{}{}</span>".format(
-                            str(substitution["timeMin"]), "'"
-                        ),
-                        unsafe_allow_html=True,
-                    )
-
-        player_stats = player_stats.loc[
-            player_stats["date"] == df_match_selected.date.values[0]
-        ]
-        player_stats = [
-            ast.literal_eval(
-                player_stats["player_stats_" + str(team)].values[match]
-            )
-            for match in range(
-                len(player_stats["player_stats_" + str(team)].values)
-            )
-        ]  # Get all player stats from all matches on the selected date
-        player_stats = [
-            item for sublist in player_stats for item in sublist
-        ]  # flatten player stats list
-
-        df_selected = pd.DataFrame(grid_table["selected_rows"])
-        if not df_selected.empty:
-            selected_player = (
-                df["playerId"]
-                .loc[df["Naam"] == str(df_selected["Naam"].values[0])]
-                .values[0]
-            )
-            player_stat = next(
-                player
-                for player in player_stats
-                if player["playerId"] == selected_player
-            )
-            player_stat = pd.DataFrame(
-                player_stat, index=[1]
-            )  # .sort_index(axis=1)
-            player_stat.rename(
-            columns={"playerName": "Naam",
-                    "saves":"Reddingen",
-                    "goalAssist": "Assists",
-                    "accuratePass": "Passes Aangekomen",
-                    "totalPass": "Totaal Passes",
-                    "score_pogingen": "Score Pogingen",
-                    "minsPlayed": "Minuten Gespeeld",
-                    "Pass_accuracy": "Pass Accuraatheid"},
-            inplace=True,
-            )
-            player_stat = player_stat.set_index("Naam")
+        if selected_formations:
             
-    try:
-        st.write(player_stat.loc[:, player_stat.columns != "playerId"])
-    except:
-        pass
+            df.rename(
+                columns={"playerName": "Naam",
+                        "position": "Positie",
+                        "positionSide": "Positie kant"},
+                inplace=True,
+            )
+            gd = GridOptionsBuilder.from_dataframe(
+                df[["Naam", "Positie", "Positie kant"]]
+            )
+            gd.configure_pagination(enabled=False)
+            gd.configure_selection(selection_mode="single", use_checkbox=True)
+            gridOptions = gd.build()
+
+            custom_css = {
+                ".ag-column-hover": {"background-color": "#Cbcbcb", "color": "#FFFFFF"},
+                ".ag-row-hover": {"background-color": "#Cbcbcb", "color": "#FFFFFF"},
+                ".ag-header-hover": {"background-color": "#Cbcbcb", "color": "#FFFFFF"},
+                ".ag-header-cell": {"background-color": "#100c44", "color": "#FFFFFF"},
+                ".ag-row-odd": {"background-color": "#100c44", "color": "#FFFFFF"},
+                ".ag-row-even": {"background-color": "#100c44", "color": "#FFFFFF"},
+                ".ag-subheader": {"border-color": "#100c44"},
+            }
+
+            grid_table = AgGrid(
+                df[["Naam", "Positie", "Positie kant"]],
+                gridOptions=gridOptions,
+                enable_enterprise_modules=False,
+                fit_columns_on_grid_load=True,
+                custom_css=custom_css,
+                update_mode=GridUpdateMode.MODEL_CHANGED,
+                allow_unsafe_jscode=True,
+                reload_data=True,
+            )
+            # Show substitutions
+            substitutions = df_match_selected["substitutions_" + str(team)].values[0]
+            nameOff, symbol, nameOn, timeMin = st.columns((4, 1, 4, 1))
+            if substitutions:
+                for substitution in ast.literal_eval(substitutions):
+                    with nameOff:
+                        st.write(
+                            "<span style='font-size:12px'>{}</span>".format(
+                                substitution["playerOffName"]
+                            )
+                            + "<span style='color:red;font-size:12px'>{}</span>".format(
+                                "⬇"
+                            ),
+                            unsafe_allow_html=True,
+                        )
+                    with symbol:
+                        st.write(
+                            "<span style='color:white;font-size:12px'>{}</span>".format(
+                                "⇆"
+                            ),
+                            unsafe_allow_html=True,
+                        )
+                    with nameOn:
+                        st.write(
+                            "<span style='font-size:12px'>{}</span>".format(
+                                substitution["playerOnName"]
+                            )
+                            + "<span style='color:green;font-size:12px'>{}</span>".format(
+                                "⬆"
+                            ),
+                            unsafe_allow_html=True,
+                        )
+                    with timeMin:
+                        st.write(
+                            "<span style='color:white;font-size:12px'>{}{}</span>".format(
+                                str(substitution["timeMin"]), "'"
+                            ),
+                            unsafe_allow_html=True,
+                        )
+
+            player_stats = player_stats.loc[
+                player_stats["date"] == df_match_selected.date.values[0]
+            ]
+            player_stats = [
+                ast.literal_eval(
+                    player_stats["player_stats_" + str(team)].values[match]
+                )
+                for match in range(
+                    len(player_stats["player_stats_" + str(team)].values)
+                )
+            ]  # Get all player stats from all matches on the selected date
+            player_stats = [
+                item for sublist in player_stats for item in sublist
+            ]  # flatten player stats list
+
+            df_selected = pd.DataFrame(grid_table["selected_rows"])
+            if not df_selected.empty:
+                selected_player = (
+                    df["playerId"]
+                    .loc[df["Naam"] == str(df_selected["Naam"].values[0])]
+                    .values[0]
+                )
+                player_stat = next(
+                    player
+                    for player in player_stats
+                    if player["playerId"] == selected_player
+                )
+                player_stat = pd.DataFrame(
+                    player_stat, index=[1]
+                )  # .sort_index(axis=1)
+                player_stat.rename(
+                columns={"playerName": "Naam",
+                        "saves":"Reddingen",
+                        "goalAssist": "Assists",
+                        "accuratePass": "Passes Aangekomen",
+                        "totalPass": "Totaal Passes",
+                        "score_pogingen": "Score Pogingen",
+                        "minsPlayed": "Minuten Gespeeld",
+                        "Pass_accuracy": "Pass Accuraatheid"},
+                inplace=True,
+                )
+                player_stat = player_stat.set_index("Naam")
+                
+        try:
+            st.write(player_stat.loc[:, player_stat.columns != "playerId"])
+        except:
+            pass
 
 
 def ST_SchotenOpDoelTeam(
