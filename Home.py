2419cf69b18e29eebe9c2a3bf5b42fce92aa1cff
marc-aurai <marc@aurai.com>
2023-05-31 10:24:34 +0200

    Format: Home.py

--- Home.py
+++ Home.py
@@ -6,8 +6,8 @@ def streamlit_page_config():
     st.set_page_config(
         page_title="Southfields AI",
         page_icon=Image.open("assets/image/SF_icon.png"),
-        layout='wide',
-        initial_sidebar_state='expanded'
+        layout="wide",
+        initial_sidebar_state="expanded",
     )
 
     hide_streamlit_style = """
@@ -17,28 +17,19 @@ def streamlit_page_config():
                 button[title="View fullscreen"]{visibility: hidden;}
                 </style>
                 """
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 
 @st.cache_data(show_spinner="Een momentje...")
 def load_images():
-    image = Image.open('assets/image/southfields_logo.png')
-    image_aurai = Image.open('assets/image/aurai_logo.png')
+    image = Image.open("assets/image/southfields_logo.png")
+    image_aurai = Image.open("assets/image/aurai_logo.png")
     return image, image_aurai
 
 
 streamlit_page_config()
-image, image_aurai = load_images() 
+image, image_aurai = load_images()
+
 st.image(image)
-
-
 st.write("# Welkom bij de AI Tools van Southfields!")
-
 st.sidebar.success("Selecteer een Tool hierboven.")
-# st.sidebar.image(image_aurai)
-
-# st.markdown(
-#     """
-#     Auteur: Marc Blomvliet (Aurai) \n
-#     **👈 Selecteer een Tool uit de sidebar** 
-# """
-# )
